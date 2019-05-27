c05dbee590cf33238efb0e41ae94f3de9128ca20
Attila Szilard Toth <ati.toth@gmail.com>
2019-05-27 18:07:57 -0400

    Send login data to the backend service via REST

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,9 +3,43 @@ import React, {Component} from 'react';
 import "../css/Login.css";
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            LoginData: {
+                username: '',
+                password: ''
+            }
+        };
 
-    handleSubmit(data) {
-        alert("Failed to log in: no back end service connected!")
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            LoginData: {
+                username: event.target.username,
+                password: event.target.password
+            }
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        fetch('http://localhost:8080/login', {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.state.LoginData)
+        }).then(function (response) {
+            return response.json();
+        }).then(function (myJson) {
+            alert(JSON.stringify(myJson));
+        });
     }
 
     render() {
@@ -13,8 +47,16 @@ class Login extends Component {
             <div className="login">
                 <div className="form">
                     <form className="login-form" onSubmit={this.handleSubmit}>
-                        <input type="text" placeholder="E-mail"/>
-                        <input type="password" placeholder="Jelszó"/>
+                        <input id="username"
+                               type="text"
+                               value={this.state.LoginData.username}
+                               onChange={this.handleChange}
+                               placeholder="E-mail"/>
+                        <input id="password"
+                               type="password"
+                               value={this.state.LoginData.password}
+                               onChange={this.handleChange}
+                               placeholder="Jelszó"/>
                         <button type="submit">Bejelentkezés</button>
                     </form>
                 </div>
