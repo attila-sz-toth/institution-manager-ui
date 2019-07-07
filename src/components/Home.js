e8289cd6a5b8cc55ce1ed58eec880334dd4f1709
Attila Szilard Toth <ati.toth@gmail.com>
2019-07-07 18:58:47 -0400

    Refactoring

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
-import AuthenticationService from "../services/AuthenticationService";
+import AuthenticationService, {ADMIN_ROLE, EMPLOYEE_ROLE} from "../services/AuthenticationService";
 import {Redirect} from "react-router-dom";
 
 class Home extends Component {
     render() {
-        if (AuthenticationService.getRole() === '[ADMIN]') {
+        if (AuthenticationService.getRole() === ADMIN_ROLE) {
             return <Redirect to="/admin-home"/>
-        } else if (AuthenticationService.getRole() === '[EMPLOYEE]') {
+        } else if (AuthenticationService.getRole() === EMPLOYEE_ROLE) {
             return <Redirect to="/employee-home"/>
         } else {
             return <Redirect to="/login"/>
